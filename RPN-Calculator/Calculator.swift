eaca5805aacad00b5f532cafcc01d6f8bfd004b2
Hayden Kua <kua.h@etoncollege.org.uk>
2020-06-10 12:58:36 +0800

    Start

---
+++ RPN-Calculator/Calculator.swift
@@ -0,0 +1,20 @@
+//
+//  Calculator.swift
+//  RPN-Calculator
+//
+//  Created by Hayden Kua on 10/06/2020.
+//  Copyright © 2020 Hayden Kua. All rights reserved.
+//
+
+import Foundation
+
+class Calculator{
+    
+    private var stack: [String]
+    
+    init(stack: [String]) {
+        self.stack = stack
+    }
+    
+    
+}
